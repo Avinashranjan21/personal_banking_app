f0201d1905d74654948050c94ef19f22c7fd17d4
Avinash Ranjan <avinash@appscelestial.com>
2025-02-09 16:04:05 +0530

    Add initial project files and configurations for Flutter application

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HomeScreen());
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Center(
+          child: Text('Hello Avinash!'),
+        ),
+      ),
+    );
+  }
+}
